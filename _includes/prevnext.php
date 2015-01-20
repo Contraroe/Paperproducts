a1e13ec97920c84b9dc32cd8b2c33b6ff70adfcf
Pieter De Norre <pieter.de.norre@telenet.be>
2015-01-20 16:51:59 +0100

    Revert 701121f..b512c40
    This rolls back to commit 701121f2ed6829e90f44164e4956c166e83898f7.

--- _includes/prevnext.php
+++ _includes/prevnext.php
@@ -1,6 +1,8 @@
 <section id="gnav">
 	<div id="cont" class="prevnext">
 		<div id="navb" class="prev"><div id="imgnav" class="imgprev"></div><?php PrevColl ($coll_id); ?></div>
-		<div id="navb" class="next"><div id="imgnav" class="imgnext"></div><?php NextColl ($coll_id); ?></div>
+<!--		<div id="navb" class="next"><div id="imgnav" class="imgnext"></div><?php NextColl ($coll_id); ?></div>   -->
+		<div id="navb" class="next"><div id="imgnav" class="imgnext"></div><?php NextCollNew ($coll_id,$coll_atel); ?></div>  
+
 	</div>
 </section>
--- _js/mouseover.js
+++ _js/mouseover.js
@@ -3,7 +3,8 @@ function rolloverpng(id,coll_id,coll_sub_id,type_id) {
 
 	
 	document.getElementById('1_1').src = '_img/collections/'  +  coll_sub_id + '.jpg';
-	document.getElementById('1_2').href = 'collections.php?coll_id='  +  coll_id + '&coll_sub_id=' + coll_sub_id + '&type_id=' + type_id;
+//	document.getElementById('1_2').href = 'collections.php?coll_id='  +  coll_id + '&coll_sub_id=' + coll_sub_id + '&type_id=' + type_id;
+	document.getElementById('1_3').src = '_img/collections/'  +  coll_sub_id + '.jpg';
 }
 
 function rolloutpng(id,coll_id,coll_sub_id,type_id) {
