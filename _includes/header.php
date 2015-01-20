b512c40cca353dbbd3fc635d7306959ebc98ff5f
Pieter De Norre <pieter.de.norre@telenet.be>
2015-01-20 16:51:12 +0100

    Revert "v1.9.5"
    This reverts commit 701121f2ed6829e90f44164e4956c166e83898f7.

--- _includes/header.php
+++ _includes/header.php
@@ -1,3 +1,3 @@
 <header >
-		<?php AllCollNew () ; ?>
+		<?php AllColl () ; ?>
 </header>
--- _includes/prevnext.php
+++ _includes/prevnext.php
@@ -1,8 +1,6 @@
 <section id="gnav">
 	<div id="cont" class="prevnext">
 		<div id="navb" class="prev"><div id="imgnav" class="imgprev"></div><?php PrevColl ($coll_id); ?></div>
-<!--		<div id="navb" class="next"><div id="imgnav" class="imgnext"></div><?php NextColl ($coll_id); ?></div>   -->
-		<div id="navb" class="next"><div id="imgnav" class="imgnext"></div><?php NextCollNew ($coll_id,$coll_atel); ?></div>  
-
+		<div id="navb" class="next"><div id="imgnav" class="imgnext"></div><?php NextColl ($coll_id); ?></div>
 	</div>
 </section>
